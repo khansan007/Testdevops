f7125b246ee8ae7d66db6ba25b3366c7a9affee8
khansan007 <khandevops0007@gmail.com>
2025-05-15 00:31:59 +0530

    samefiletestingudpate

--- waiting.py
+++ waiting.py
@@ -8,3 +8,11 @@ I love you more then anythign ya rabb
 You are my rabb
 you are muy rabbana 
 You are my creator and you are my everything
+
+Ya Malik ul Mulk 
+You know what is in my heart 
+You know what all i feel for you
+I want to convince you and ony you 
+no one else is worth Ya rabb
+I love you My rab my rabbana
+
