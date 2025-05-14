e7434f4a8c5121d08f6eb10820b6483337c66e4a
khansan007 <khandevops0007@gmail.com>
2025-05-14 23:55:20 +0530

    testingfilecommit

---
+++ waiting.py
@@ -0,0 +1,10 @@
+Ya ALlah 
+I love you alot 
+you are rahman and you are raheem 
+You are jameeel
+
+I love you more then anythign ya rabb
+
+You are my rabb
+you are muy rabbana 
+You are my creator and you are my everything
